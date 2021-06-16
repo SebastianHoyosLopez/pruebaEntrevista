8d7b25b60c32d1f0c9d6919e626ba6c7f2fa0b60
sebastianhoyoslopez <sebastianhoyos145@gmail.com>
2021-06-16 16:24:00 -0500

    proyecto terminado

--- src/component/footer/FooterHome.jsx
+++ src/component/footer/FooterHome.jsx
@@ -3,24 +3,60 @@ import "./footer.css";
 
 const FooterHome = () => {
   return (
-    <footer id="footerHome" style={{ color: "yellow", background: "#655E5D" }}>
+    <section id="footer">
       <div className="container">
-        <div className="row my-4">
-          <div className="col">
-            <h6>SOBRE NOSOTROS</h6>
+        <div className="row text-center text-xs-center text-sm-left text-md-left">
+          <div className="col-xs-12 col-sm-4 col-md-4">
+            <h5>Quick links</h5>
+            <ul className="list-unstyled quick-links">
+              <li><a href="https://www.google.com/"><i className="fa fa-angle-double-right"></i>Home</a></li>
+              <li><a href="https://www.google.com/"><i className="fa fa-angle-double-right"></i>About</a></li>
+              <li><a href="https://www.google.com/"><i className="fa fa-angle-double-right"></i>FAQ</a></li>
+              <li><a href="https://www.google.com/"><i className="fa fa-angle-double-right"></i>Get Started</a></li>
+              <li><a href="https://www.google.com/"><i className="fa fa-angle-double-right"></i>Videos</a></li>
+            </ul>
           </div>
-          <div className="col">
-            <h6>CATEGORÍAS DESTACADAS</h6>
+          <div className="col-xs-12 col-sm-4 col-md-4">
+            <h5>Quick links</h5>
+            <ul className="list-unstyled quick-links">
+              <li><a href="https://www.google.com/"><i className="fa fa-angle-double-right"></i>Home</a></li>
+              <li><a href="https://www.google.com/"><i className="fa fa-angle-double-right"></i>About</a></li>
+              <li><a href="https://www.google.com/"><i className="fa fa-angle-double-right"></i>FAQ</a></li>
+              <li><a href="https://www.google.com/"><i className="fa fa-angle-double-right"></i>Get Started</a></li>
+              <li><a href="https://www.google.com/"><i className="fa fa-angle-double-right"></i>Videos</a></li>
+            </ul>
           </div>
-          <div className="col">
-            <h6>SERVICIO AL CLIENTE</h6>
-          </div>
-          <div className="col">
-            <h6>SUSCRÍBASE A NUESTRO NEWSLETTER</h6>
+          <div className="col-xs-12 col-sm-4 col-md-4">
+            <h5>Quick links</h5>
+            <ul className="list-unstyled quick-links">
+              <li><a href="https://www.google.com/"><i className="fa fa-angle-double-right"></i>Home</a></li>
+              <li><a href="https://www.google.com/"><i className="fa fa-angle-double-right"></i>About</a></li>
+              <li><a href="https://www.google.com/"><i className="fa fa-angle-double-right"></i>FAQ</a></li>
+              <li><a href="https://www.google.com/"><i className="fa fa-angle-double-right"></i>Get Started</a></li>
+              <li><a href="https://wwwe.sunlimetech.com" title="Design and developed by"><i className="fa fa-angle-double-right"></i>Imprint</a></li>
+            </ul>
           </div>
         </div>
+        <div className="row">
+          <div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+            <ul className="list-unstyled list-inline social text-center">
+              <li className="list-inline-item"><a href="https://www.google.com/"><i className="fa fa-facebook"></i></a></li>
+              <li className="list-inline-item"><a href="https://www.google.com/"><i className="fa fa-twitter"></i></a></li>
+              <li className="list-inline-item"><a href="https://www.google.com/"><i className="fa fa-instagram"></i></a></li>
+              <li className="list-inline-item"><a href="https://www.google.com/"><i className="fa fa-google-plus"></i></a></li>
+              <li className="list-inline-item"><a href="https://www.google.com/" target="_blank"><i className="fa fa-envelope"></i></a></li>
+            </ul>
+          </div>
+          <hr/>
+        </div>	
+        <div className="row">
+          <div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+            <p className="h6">© All right Reversed</p>
+          </div>
+          <hr/>
+        </div>	
       </div>
-    </footer>
+    </section>
   );
 };
 
